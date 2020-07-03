8e57d765b29836b5ca5b253b7129d4d3d41b0957
Pedro <pvdo-carmo-saraiva-t@mysenenca.ca>
2020-07-03 04:26:08 -0400

    commit

--- data.js
+++ data.js
@@ -58,3 +58,134 @@ module.exports.getData = function () {
 
   return meals;
 };
+module.exports.login = (body) => {
+  return new Promise((resolve, reject) => {
+    var returnObj = {
+      email: body.logEmail,
+      pass: body.logPassword,
+      emailError: false,
+      passError: false,
+    };
+    //Check email && password
+    if (!body.logEmail && !body.logPassword) {
+      returnObj.emailError = true;
+      returnObj.passError = true;
+      reject(returnObj);
+      return;
+    }
+    //Check email
+    if (body.logEmail == "") {
+      returnObj.emailError = true;
+      reject(returnObj);
+      return;
+    }
+    //Check Password
+    if (!body.logPassword) {
+      returnObj.passError = true;
+      reject(returnObj);
+      return;
+    }
+    //All good
+    if (body.logEmail && body.logPassword) {
+      returnObj.emailError = false;
+      returnObj.passError = false;
+      resolve();
+      return;
+    }
+  });
+};
+
+//Register
+module.exports.register = (body) => {
+  return new Promise((resolve, reject) => {
+    var returnObj = {
+      //Values
+      fname: body.fName,
+      lname: body.lName,
+      email: body.regEmail,
+      pass: body.regPass,
+      repeatPass: body.repeatPass,
+      //Errors Flags
+      fnameError: false,
+      lnameError: false,
+      emailError: {
+        empty: false,
+        isEmail: false,
+      },
+      passError: {
+        box: false,
+        empty: false,
+        charLength: false,
+        capital: false,
+        digit: false,
+        specialChar: false,
+      },
+      repeatPassError: false,
+    };
+
+    var flag = false;
+    //Check First Name is empty
+    if (!returnObj.fname) {
+      returnObj.fnameError = true;
+      flag = true;
+    }
+    //Check Last Name is empty
+    if (!body.lName) {
+      returnObj.lnameError = true;
+      flag = true;
+    }
+    //Check email is empty
+    if (!body.regEmail) {
+      returnObj.emailError.empty = true;
+      flag = true;
+    }
+    //Check if it is an email
+    if (!body.regEmail.match(/[\w-]+@([\w-]+\.)+[\w-]+/)) {
+      returnObj.emailError.isEmail = true;
+      flag = true;
+    }
+    //Check password is empty
+    if (!body.regPass) {
+      returnObj.passError.empty = true;
+      returnObj.passError.box = true;
+      flag = true;
+    }
+    //Check password length
+    if (body.regPass.length < 8) {
+      returnObj.passError.charLength = true;
+      returnObj.passError.box = true;
+      flag = true;
+    }
+    //Check password have Capital letter
+    if (!body.regPass.match(/[A-Z]/)) {
+      returnObj.passError.capital = true;
+      returnObj.passError.box = true;
+      flag = true;
+    }
+    //Check password have digit
+    if (!body.regPass.match(/[0-9]/)) {
+      returnObj.passError.digit = true;
+      returnObj.passError.box = true;
+      flag = true;
+    }
+    //Check password have special character
+    if (!body.regPass.match(/[!@#\$%\^\&*\)\(?]/)) {
+      returnObj.passError.specialChar = true;
+      returnObj.passError.box = true;
+      flag = true;
+    }
+    //Check if the password is repeated
+    if (!body.repeatPass.match(body.regPass)) {
+      returnObj.repeatPassError = true;
+      flag = true;
+    }
+
+    if (flag == true) {
+      reject(returnObj);
+      return;
+    } else {
+      resolve(returnObj);
+      return;
+    }
+  });
+};
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express"); //Load Express.js
 const app = express();
 const ds = require("./data.js"); //Load data.js
 const expressHB = require("express-handlebars"); //Load Express hbs
+const bodyParser = require("body-parser");
+var nodemailer = require("nodemailer");
 
-const HTTP_PORT = process.env.PORT || 8080; //
+const HTTP_PORT = process.env.PORT || 8081; //
 
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
@@ -19,6 +21,7 @@ app.set("view engine", ".hbs");
 // ------Access to images
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 // ------ ROUTING
 // ------Respond the GET with the webpages
 
@@ -32,13 +35,64 @@ app.get("/meals", (req, res) => {
   res.render("meals", { data: mealsData });
 });
 
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+app.get("/form", (req, res) => {
+  res.render("form");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 // ------- Respond to the submitted forms
-app.post("/loginSub", (req, res) => {
-  res.redirect("/");
+// app.post("/submit", (req, res) => {
+//   res.redirect("/dashboard");
+// });
+
+//Login post
+app.post("/form", (req, res) => {
+  ds.login(req.body)
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((inData) => {
+      res.render("form", { message: inData });
+    });
 });
 
-app.post("/registrationSub", (req, res) => {
-  res.redirect("/");
+//Register Post
+app.post("/register", (req, res) => {
+  ds.register(req.body)
+    .then((inData) => {
+      //SEND EMAIL
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "bettermealsfake@gmail.com",
+          pass: "Senha1234",
+        },
+      });
+
+      var mailOptions = {
+        from: "bettermealsfake@gmail.com",
+        to: req.body.regEmail,
+        subject: "Welcome",
+        text: "We are glad to see you here!!",
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      //Redirect to dashboard
+      res.render("dashboard", { user: inData });
+    })
+    .catch((inData) => {
+      res.render("register", { reg: inData });
+    });
 });
 
 // app.use((req, res) => {
